04a9ec5ae10f32424e5cbfb43aca33e247486d74
murgo <murgo@iki.fi>
2011-03-18 02:22:07 +0200

    added forgotten files

---
+++ src/main/java/drinkcounter/model/ShotFunction.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package drinkcounter.model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author murgo
+ */
+public class ShotFunction {
+    private double a;
+    private double b;
+    private double increment;
+    private double cutter;
+
+    public ShotFunction(Date timeTaken, double burnRate, double i)
+    {
+        a = burnRate;
+        increment = i;
+
+        b = -burnRate * timeTaken.getTime() + increment;
+    }
+
+    public double getCutter() { return cutter; }
+    public void setCutter(double c) { cutter = c; }
+
+    public double calc(long seconds)
+    {
+        double result = a * seconds + b;
+
+        if (result > increment)
+            return 0;
+
+        if (result <= cutter)
+            return 0;
+
+        return result;
+    }
+
+    public double calc(Date t)
+    {
+        return calc(t.getTime());
+    }
+}
