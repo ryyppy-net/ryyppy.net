c8d9478263fbd42a13b3e7307cd889417112c1ff
Ville Salonen <ville.salonen@iki.fi>
2011-04-24 11:22:44 +0300

    Formatted the code in AuthenticationService.java.

--- src/main/java/drinkcounter/authentication/AuthenticationService.java
+++ src/main/java/drinkcounter/authentication/AuthenticationService.java
@@ -32,7 +32,8 @@ import org.slf4j.LoggerFactory;
  */
 @Component
 public class AuthenticationService {
-	private static final Logger log = LoggerFactory.getLogger(AuthenticationService.class);
+
+    private static final Logger log = LoggerFactory.getLogger(AuthenticationService.class);
     private ConsumerManager consumerManager;
 
     public AuthenticationService() {
@@ -40,123 +41,121 @@ public class AuthenticationService {
             consumerManager = new ConsumerManager();
             consumerManager.setAssociations(new InMemoryConsumerAssociationStore());
             consumerManager.setNonceVerifier(new InMemoryNonceVerifier(10000));
-		} catch (ConsumerException e) {
-			String message = "Exception creating ConsumerManager!";
-			log.error(message, e);
-			throw new RuntimeException(message, e);
-		}
+        } catch (ConsumerException e) {
+            String message = "Exception creating ConsumerManager!";
+            log.error(message, e);
+            throw new RuntimeException(message, e);
+        }
     }
 
+    /**
+     * Perform discovery on the User-Supplied identifier and return the
+     * DiscoveryInformation object that results from Association with the
+     * OP. This will probably be needed by the caller (stored in Session
+     * perhaps?).
+     * 
+     * I'm not thrilled about ConsumerManager being static, but it is
+     * very important to openid4java that the ConsumerManager object be the
+     * same instance all through a conversation (discovery, auth request, 
+     * auth response) with the OP. I didn't dig terribly deeply, but suspect
+     * that part of the key exchange or the nonce uses the ConsumerManager's
+     * hash, or some other instance-specific construct to do its thing.
+     * 
+     * @param userSuppliedIdentifier The User-Supplied identifier. It may already
+     *  be normalized.
+     *
+     *  @return DiscoveryInformation - The resulting DisoveryInformation object
+     *  returned by openid4java following successful association with the OP.
+     */
+    @SuppressWarnings("unchecked")
+    public DiscoveryInformation performDiscoveryOnUserSuppliedIdentifier(String userSuppliedIdentifier) {
+        DiscoveryInformation ret = null;
+        try {
+            // Perform discover on the User-Supplied Identifier
+            List<DiscoveryInformation> discoveries = consumerManager.discover(userSuppliedIdentifier);
+            // Pass the discoveries to the associate() method...
+            ret = consumerManager.associate(discoveries);
+
+        } catch (DiscoveryException e) {
+            String message = "Error occurred during discovery!";
+            log.error(message, e);
+            throw new RuntimeException(message, e);
+        }
+        return ret;
+    }
+
+    /**
+     * Create an OpenID Auth Request, using the DiscoveryInformation object
+     * return by the openid4java library.
+     * 
+     * This method also uses the Simple Registration Extension to grant
+     * the Relying Party (RP).
+     * 
+     * @param discoveryInformation The DiscoveryInformation that should have
+     *  been previously obtained from a call to 
+     *  performDiscoveryOnUserSuppliedIdentifier().
+     *  
+     *  @param returnToUrl The URL to which the OP will redirect once the
+     *   authentication call is complete.
+     *  
+     * @return AuthRequest - A "good-to-go" AuthRequest object packed with all
+     *  kinds of great OpenID goodies for the OpenID Provider (OP). The caller
+     *  must take this object and forward it on to the OP. Or call
+     *  processAuthRequest() - part of this Service Class.
+     */
+    public AuthRequest createOpenIdAuthRequest(DiscoveryInformation discoveryInformation, String returnToUrl) {
+        AuthRequest ret = null;
+        //
+        try {
+            // Create the AuthRequest object
+            ret = consumerManager.authenticate(discoveryInformation, returnToUrl);
 
 
-	
-	/**
-	 * Perform discovery on the User-Supplied identifier and return the
-	 * DiscoveryInformation object that results from Association with the
-	 * OP. This will probably be needed by the caller (stored in Session
-	 * perhaps?).
-	 * 
-	 * I'm not thrilled about ConsumerManager being static, but it is
-	 * very important to openid4java that the ConsumerManager object be the
-	 * same instance all through a conversation (discovery, auth request, 
-	 * auth response) with the OP. I didn't dig terribly deeply, but suspect
-	 * that part of the key exchange or the nonce uses the ConsumerManager's
-	 * hash, or some other instance-specific construct to do its thing.
-	 * 
-	 * @param userSuppliedIdentifier The User-Supplied identifier. It may already
-	 *  be normalized.
-	 *
-	 *  @return DiscoveryInformation - The resulting DisoveryInformation object
-	 *  returned by openid4java following successful association with the OP.
-	 */
-	@SuppressWarnings("unchecked")
-	public DiscoveryInformation performDiscoveryOnUserSuppliedIdentifier(String userSuppliedIdentifier) {
-		DiscoveryInformation ret = null;
-		try {
-		// Perform discover on the User-Supplied Identifier
-		List<DiscoveryInformation> discoveries = consumerManager.discover(userSuppliedIdentifier);
-		// Pass the discoveries to the associate() method...
-		ret = consumerManager.associate(discoveries);
+        } catch (Exception e) {
+            String message = "Exception occurred while building AuthRequest object!";
+            log.error(message, e);
+            throw new RuntimeException(message, e);
+        }
+        return ret;
+    }
 
-		} catch (DiscoveryException e) {
-			String message = "Error occurred during discovery!";
-			log.error(message, e);
-			throw new RuntimeException(message, e);
-		}
-		return ret;
-	}
-	/**
-	 * Create an OpenID Auth Request, using the DiscoveryInformation object
-	 * return by the openid4java library.
-	 * 
-	 * This method also uses the Simple Registration Extension to grant
-	 * the Relying Party (RP).
-	 * 
-	 * @param discoveryInformation The DiscoveryInformation that should have
-	 *  been previously obtained from a call to 
-	 *  performDiscoveryOnUserSuppliedIdentifier().
-	 *  
-	 *  @param returnToUrl The URL to which the OP will redirect once the
-	 *   authentication call is complete.
-	 *  
-	 * @return AuthRequest - A "good-to-go" AuthRequest object packed with all
-	 *  kinds of great OpenID goodies for the OpenID Provider (OP). The caller
-	 *  must take this object and forward it on to the OP. Or call
-	 *  processAuthRequest() - part of this Service Class.
-	 */
-	public AuthRequest createOpenIdAuthRequest(DiscoveryInformation discoveryInformation, String returnToUrl) {
-		AuthRequest ret = null;
-		//
-		try {
-			// Create the AuthRequest object
-			ret = consumerManager.authenticate(discoveryInformation, returnToUrl);
-
-			
-		} catch (Exception e) {
-			String message = "Exception occurred while building AuthRequest object!";
-			log.error(message, e);
-			throw new RuntimeException(message, e);
-		}
-		return ret;
-	}
-	
-	/**
-	 * Processes the returned information from an authentication request
-	 * from the OP.
-	 * 
-	 * @param discoveryInformation DiscoveryInformation that was created earlier
-	 *  in the conversation (by openid4java). This will need to be verified with
-	 *  openid4java to make sure everything went smoothly and there are no
-	 *  possible problems. This object was probably stored in session and retrieved
-	 *  for use in calling this method.
-	 *  
-	 * @param pageParameters PageParameters passed to the page handling the
-	 *  return verificaion.
-	 *  
-	 * @param returnToUrl The "return to" URL that was passed to the OP. It must
-	 *  match exactly, or openid4java will issue a verification failed message
-	 *  in the logs.
-	 *  
-	 * @return openId of the dude if authenticated, null otherwise
-	 */
-	public String processReturn(DiscoveryInformation discoveryInformation, Map<String, String> pageParameters, String returnToUrl) {
-            String openId = null;
-            // Verify the Information returned from the OP
-            /// This is required according to the spec
-            ParameterList response = new ParameterList(pageParameters);
-            try {
-                    VerificationResult verificationResult = consumerManager.verify(returnToUrl, response, discoveryInformation);
-                    Identifier verifiedIdentifier = verificationResult.getVerifiedId();
-                    AuthSuccess authSuccess = (AuthSuccess)verificationResult.getAuthResponse();
-                    if (verifiedIdentifier != null && authSuccess != null) {
-                            openId = verifiedIdentifier.getIdentifier();
-                    }
-            } catch (Exception e) {
-                    String message = "Exception occurred while verifying response!";
-                    log.error(message, e);
-                    throw new RuntimeException(message, e);
+    /**
+     * Processes the returned information from an authentication request
+     * from the OP.
+     * 
+     * @param discoveryInformation DiscoveryInformation that was created earlier
+     *  in the conversation (by openid4java). This will need to be verified with
+     *  openid4java to make sure everything went smoothly and there are no
+     *  possible problems. This object was probably stored in session and retrieved
+     *  for use in calling this method.
+     *  
+     * @param pageParameters PageParameters passed to the page handling the
+     *  return verificaion.
+     *  
+     * @param returnToUrl The "return to" URL that was passed to the OP. It must
+     *  match exactly, or openid4java will issue a verification failed message
+     *  in the logs.
+     *  
+     * @return openId of the dude if authenticated, null otherwise
+     */
+    public String processReturn(DiscoveryInformation discoveryInformation, Map<String, String> pageParameters, String returnToUrl) {
+        String openId = null;
+        // Verify the Information returned from the OP
+        /// This is required according to the spec
+        ParameterList response = new ParameterList(pageParameters);
+        try {
+            VerificationResult verificationResult = consumerManager.verify(returnToUrl, response, discoveryInformation);
+            Identifier verifiedIdentifier = verificationResult.getVerifiedId();
+            AuthSuccess authSuccess = (AuthSuccess) verificationResult.getAuthResponse();
+            if (verifiedIdentifier != null && authSuccess != null) {
+                openId = verifiedIdentifier.getIdentifier();
             }
-            
-            return openId;
-	}
+        } catch (Exception e) {
+            String message = "Exception occurred while verifying response!";
+            log.error(message, e);
+            throw new RuntimeException(message, e);
+        }
+
+        return openId;
+    }
 }
