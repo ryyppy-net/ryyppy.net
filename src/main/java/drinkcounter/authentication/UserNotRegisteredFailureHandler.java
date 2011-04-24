2a38661a7ddabf87019e2ecb1bf51ab44e9aee86
Toni Paloniemi <toni.paloniemi@gmail.com>
2011-04-24 21:29:42 +0300

    authentication using spring security

---
+++ src/main/java/drinkcounter/authentication/UserNotRegisteredFailureHandler.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package drinkcounter.authentication;
+
+import drinkcounter.web.controllers.ui.AuthenticationController;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.openid.OpenIDAuthenticationStatus;
+import org.springframework.security.openid.OpenIDAuthenticationToken;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+
+/**
+ *
+ * @author Toni
+ */
+public class UserNotRegisteredFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+    
+    private String registrationURL = "/ui/newuser";
+
+    public void setRegistrationURL(String registrationURL) {
+        this.registrationURL = registrationURL;
+    }
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        
+        if (exception instanceof UsernameNotFoundException
+                && exception.getAuthentication() instanceof OpenIDAuthenticationToken
+                && ((OpenIDAuthenticationToken) exception.getAuthentication()).getStatus().equals(OpenIDAuthenticationStatus.SUCCESS)) {
+            DefaultRedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+            request.getSession(true).setAttribute(AuthenticationController.OPENID, ((UsernameNotFoundException) exception).getExtraInformation());
+            // redirect to registration
+            redirectStrategy.sendRedirect(request, response, registrationURL);
+
+        } else {
+            super.onAuthenticationFailure(request, response, exception);
+        }
+    }
+}
