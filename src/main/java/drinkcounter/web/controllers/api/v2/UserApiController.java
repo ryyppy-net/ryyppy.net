9a45cd47938ccc241761dac26a5927857dcbbabc
Toni Paloniemi <toni.paloniemi@gmail.com>
2012-02-11 16:33:59 +0200

    /users

--- src/main/java/drinkcounter/web/controllers/api/v2/UserApiController.java
+++ src/main/java/drinkcounter/web/controllers/api/v2/UserApiController.java
@@ -6,7 +6,14 @@ package drinkcounter.web.controllers.api.v2;
 
 import com.google.gson.Gson;
 import drinkcounter.UserService;
+import drinkcounter.authentication.CurrentUser;
 import drinkcounter.model.User;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import javax.servlet.http.HttpSession;
+import org.apache.commons.collections.MapUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,16 +32,24 @@ public class UserApiController {
     @Autowired
     private UserService userService;
     
+    @Autowired
+    private CurrentUser currentUser;
+    
     private Gson gson = new Gson();
     
     @RequestMapping(value="/users/{userId}", method=RequestMethod.GET)
     public @ResponseBody String getUser(@PathVariable Integer userId) {
         User user = userService.getUser(userId);
-        UserDTO userDTO = new UserDTO();
-        userDTO.setId(user.getId());
-        userDTO.setName(user.getName());
-        userDTO.setEmail(user.getEmail());
-        userDTO.setSex(user.getSex());
+        UserDTO userDTO = UserDTO.fromUser(user);
         return gson.toJson(userDTO);
     }
+    
+    @RequestMapping(value="/users", method= RequestMethod.GET)
+    public @ResponseBody String getUsers(){
+        User user = currentUser.getUser();
+        Map<String, Integer[]> users = Collections.singletonMap("users", new Integer[]{user.getId()});
+        return gson.toJson(users);
+    }
+    
+    
 }
