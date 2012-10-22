bf1b8cdf98bb0a5c9dbb66264d075c6e2812d596
Ville Salonen <ville.salonen@iki.fi>
2012-10-22 19:43:18 +0300

    Added a Ember-based version of new UI.

---
+++ src/main/webapp/static/ember/test/unit/ryyppy.Drinker.spec.js
@@ -0,0 +1,15 @@
+describe('Drinker module', function () {
+    it('Construction', function () {
+        var drinker = Drinker.create();
+        expect(drinker).not.toBeUndefined();
+        expect(drinker.get('name')).toBe('Anonymous');
+        expect(drinker.get('drinks')).toBe(0);
+    });
+
+    it('Having drinks', function () {
+        var drinker = new Drinker();
+        expect(drinker.get('drinks')).toBe(0);
+        drinker.drink();
+        expect(drinker.get('drinks')).toBe(1);
+    });
+});
