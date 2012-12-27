9fae0bafb80110b50fb3e373e95a1b85ed5de12a
Ville Salonen <ville.salonen@iki.fi>
2012-12-27 11:52:09 +0200

    Renamed myApp to ryyppy.

--- src/main/webapp/static/angular/app/js/filters.js
+++ src/main/webapp/static/angular/app/js/filters.js
@@ -2,7 +2,7 @@
 
 /* Filters */
 
-angular.module('myApp.filters', []).
+angular.module('ryyppy.filters', []).
   filter('interpolate', ['version', function(version) {
     return function(text) {
       return String(text).replace(/\%VERSION\%/mg, version);
