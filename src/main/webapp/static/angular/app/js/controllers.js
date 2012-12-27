71c693b945ef2e4cc2f8bde9e00d786c39bba696
Ville Salonen <ville.salonen@iki.fi>
2012-12-27 12:10:25 +0200

    Added a listing of parties.

--- src/main/webapp/static/angular/app/js/controllers.js
+++ src/main/webapp/static/angular/app/js/controllers.js
@@ -3,8 +3,13 @@
 /* Controllers */
 
 
-function MyCtrl1() {}
-MyCtrl1.$inject = [];
+function MyCtrl1($scope, $http)
+{
+    $http.get("/API/v2/parties").success(function (data) {
+        $scope.parties = data;
+    });
+}
+MyCtrl1.$inject = ['$scope', '$http'];
 
 
 function MyCtrl2() {
