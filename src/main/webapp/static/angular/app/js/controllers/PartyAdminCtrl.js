e606c2c7d535ff3812ddc7a8aa4b50f59fe6072e
Ville Salonen <ville.salonen@iki.fi>
2012-12-28 20:40:11 +0200

    Party admin now show success message and updates participants after changes.

--- src/main/webapp/static/angular/app/js/controllers/PartyAdminCtrl.js
+++ src/main/webapp/static/angular/app/js/controllers/PartyAdminCtrl.js
@@ -1,45 +1,68 @@
 "use strict";
 
 function PartyAdminCtrl($scope, $routeParams, RyyppyAPI) {
-    $scope.active = "admin";
+    var self = this;
 
-    RyyppyAPI.getParty($routeParams.partyId, function (data) {
-        $scope.party = data;
-    });
-    RyyppyAPI.getPartyParticipants($routeParams.partyId, function (data) {
-        $scope.participants = data;
-    });
+    this._updatePartyInformation = function () {
+        RyyppyAPI.getParty($routeParams.partyId, function (data) {
+            $scope.party = data;
+        });
+    };
+
+    this._updatePartyParticipants = function () {
+        RyyppyAPI.getPartyParticipants($routeParams.partyId, function (data) {
+            $scope.participants = data;
+        });
+    };
+
+    this.addUser = function () {
+        if ($scope.selectedUserTypeId == 1)
+            self._addRegisteredUser($routeParams.partyId, $scope.email);
+        else
+            self._addGuestUser($scope.name, $scope.sex, $scope.weight);
+    };
+
+    this._addRegisteredUser = function (partyId, email) {
+        RyyppyAPI.addRegisteredUserToParty(partyId, email, function (data) {
+            $scope.successMessage = "Lisätty käyttäjä sähköpostiosoitteella " + email + "!";
+            self._updatePartyParticipants();
+        });
+    };
+
+    this._addGuestUser = function (name, sex, weight) {
+        var guest = { name: name, sex: sex, weight: weight };
+        RyyppyAPI.addGuestToParty($routeParams.partyId, guest, function (data) {
+            $scope.successMessage = "Lisätty vieras " + $scope.name + "!";
+            self._updatePartyParticipants();
+        });
+    }
+
+    this.removeUser = function (participant) {
+        RyyppyAPI.removeUser($routeParams.partyId, participant, function (data) {
+            $scope.successMessage = "Poistettu käyttäjä " + participant.name + "!";
+            self._updatePartyParticipants();
+        });
+    };
+
+
+    $scope.active = "admin";
 
     $scope.email = null;
     $scope.name = null;
     $scope.sex = 'MALE';
     $scope.weight = null;
 
-    $scope.addUser = function () {
-        if ($scope.selectedUserTypeId == 1) {
-            RyyppyAPI.addRegisteredUserToParty($routeParams.partyId, $scope.email, function (data) {
-                alert($scope.email);
-            });
-        }
-        else {
-            var guest = { name: $scope.name, sex: $scope.sex, weight: $scope.weight };
-            RyyppyAPI.addGuestToParty($routeParams.partyId, guest, function (data) {
-                alert($scope.name + $scope.sex + $scope.weight);
-            });
-        }
-    };
-
-    $scope.removeUser = function (participant) {
-        RyyppyAPI.removeUser($routeParams.partyId, participant, function (data) {
-            alert($scope.email);
-        });
-    };
-
     $scope.userTypes = [
         {TypeId: 1, TypeName: 'Rekisteröitynyt käyttäjä'},
         {TypeId: 2, TypeName: 'Vieras'}
     ];
     $scope.selectedUserTypeId = 1;
+
+    $scope.addUser = this.addUser;
+    $scope.removeUser = this.removeUser;
+
+    this._updatePartyInformation();
+    this._updatePartyParticipants();
 }
 
 PartyAdminCtrl.$inject = ['$scope', '$routeParams', 'RyyppyAPI'];
