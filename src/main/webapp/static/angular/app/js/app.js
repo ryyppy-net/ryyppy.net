d6f14fe8e9cebf21d6db2f5ac75ac32843182dbd
Ville Salonen <ville.salonen@iki.fi>
2012-12-29 10:44:23 +0200

    Fixed: Link to user page no longer causes a refresh.

--- src/main/webapp/static/angular/app/js/app.js
+++ src/main/webapp/static/angular/app/js/app.js
@@ -3,8 +3,8 @@
 // Declare app level module which depends on filters, and services
 angular.module('ryyppy', ['ryyppy.filters', 'ryyppy.services', 'ryyppy.directives']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('', {templateUrl: 'partials/user.html', controller: UserCtrl});
+    $routeProvider.when('/', {templateUrl: 'partials/user.html', controller: UserCtrl});
     $routeProvider.when('/party/:partyId', {templateUrl: 'partials/party.html', controller: PartyCtrl});
     $routeProvider.when('/party-admin/:partyId', {templateUrl: 'partials/party_admin.html', controller: PartyAdminCtrl});
-    $routeProvider.otherwise({redirectTo: ''});
+    $routeProvider.otherwise({redirectTo: '/'});
   }]);
