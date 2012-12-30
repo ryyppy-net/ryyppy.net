94a3e2808b98467e5e9d75668d2492f2209532ce
Ville Salonen <ville.salonen@iki.fi>
2012-12-30 09:47:22 +0200

    Fixed testing configuration to exclude unnecessary libraries but to include AngularJS.

--- src/main/webapp/static/angular/config/testacular.conf.js
+++ src/main/webapp/static/angular/config/testacular.conf.js
@@ -3,11 +3,17 @@ basePath = '../';
 files = [
   JASMINE,
   JASMINE_ADAPTER,
-  'app/lib/angular/angular.js',
-  'app/lib/angular/angular-*.js',
+  'app/js/lib/angular/angular.js',
+  'app/js/lib/angular/angular-*.js',
   'test/lib/angular/angular-mocks.js',
   'app/js/**/*.js',
   'test/unit/**/*.js'
+];
+
+exclude = [
+    'app/js/lib/flot/*.js',
+    'app/js/lib/jquery/*.js',
+    'app/js/lib/pnotify/*.js'
 ];
 
 autoWatch = true;
