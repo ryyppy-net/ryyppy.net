9bda76f1e18e1f1c472d47f62ad9ab2f6a2a2679
Toni Paloniemi <toni.paloniemi@gmail.com>
2013-07-08 17:50:39 +0300

    Removed depracations

--- src/main/java/drinkcounter/authentication/UserDetailsServiceImpl.java
+++ src/main/java/drinkcounter/authentication/UserDetailsServiceImpl.java
@@ -6,13 +6,14 @@ package drinkcounter.authentication;
 
 import drinkcounter.UserService;
 import drinkcounter.model.User;
-import java.util.Collections;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
-import org.springframework.security.core.authority.GrantedAuthorityImpl;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Collections;
 
 /**
  *
@@ -27,7 +28,7 @@ public class UserDetailsServiceImpl implements UserDetailsService{
     public UserDetails loadUserByUsername(String openId) throws UsernameNotFoundException, DataAccessException {
         User user = userService.getUserByOpenId(openId);
         if(user == null){
-            throw new UsernameNotFoundException("User with openid "+openId+" doesn't exist", openId);
+            throw new UsernameNotFoundException("User with openid "+openId+" doesn't exist");
         }
         return new DrinkcounterUserDetails(user.getOpenId(), 
                 "", 
@@ -35,7 +36,7 @@ public class UserDetailsServiceImpl implements UserDetailsService{
                 true, 
                 true, 
                 true, 
-                Collections.singleton(new GrantedAuthorityImpl("ROLE_USER")),
+                Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")),
                 user.getId());
     }
     
